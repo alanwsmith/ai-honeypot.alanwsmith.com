f5145c625e18ae783aeb9d8ab66d34d51c1bbd74
Alan Smith <commit@alanwsmith.com>
2025-04-13 14:15:23 -0400

    Workingon titles

--- ai_honeypot_builder/src/main.rs
+++ ai_honeypot_builder/src/main.rs
@@ -4,33 +4,76 @@ use dirs::document_dir;
 use markov::Chain;
 use std::path::PathBuf;
 
+#[derive(Debug)]
 struct Page {
-    file_name: String,
+    url_key: String,
     title: String,
     paragraphs: Vec<String>,
 }
 
 impl Page {
     pub fn new(harvard_chain: &Chain<String>) -> Page {
+        let title = Self::title(&harvard_chain);
+        let url_key = title.to_lowercase().replace(" ", "-");
         let page = Page {
-            file_name: "some-title".to_string(),
-            title: "Some Title".to_string(),
-            paragraphs: vec![],
+            url_key,
+            title,
+            paragraphs: Self::paragraphs(&harvard_chain),
         };
         page
+    }
+
+    fn title(harvard_chain: &Chain<String>) -> String {
+        let base_string = harvard_chain.generate_str().replace(".", "");
+        let mut title_words: Vec<&str> = base_string.split_whitespace().collect();
+        title_words.truncate(5);
+        let title: String = title_words
+            .iter()
+            .map(|word| word.to_string())
+            .collect::<Vec<String>>()
+            .join(" ");
+        dbg!(&title);
+        //title_words
+
+        //.replace(".", "");
+        //  .split_whitespace()
+        // .collect::<Vec<&str>>();
+        // dbg!(&title_words);
+
+        //title
+        "asdf".to_string()
+    }
+
+    fn paragraphs(harvard_chain: &Chain<String>) -> Vec<String> {
+        vec![
+            Self::paragraph(&harvard_chain),
+            Self::paragraph(&harvard_chain),
+            Self::paragraph(&harvard_chain),
+            Self::paragraph(&harvard_chain),
+        ]
+    }
+
+    fn paragraph(harvard_chain: &Chain<String>) -> String {
+        Self::sentence(&harvard_chain)
+    }
+
+    fn sentence(harvard_chain: &Chain<String>) -> String {
+        harvard_chain.generate_str()
     }
 }
 
 fn main() -> Result<()> {
     let base_dir = document_dir().unwrap().join("ai-honeypots");
     build_site(&base_dir, 1)?;
-    // println!("{}", harvard_chain.generate_str());
     Ok(())
 }
 
 fn build_site(base_dir: &PathBuf, id: usize) -> Result<()> {
     let output_root = base_dir.join(format!("{}", id));
     mkdir_p(&output_root)?;
+    let harvard_chain = make_harvard_chain()?;
+    let p = Page::new(&harvard_chain);
+    dbg!(p);
     Ok(())
 }
 
